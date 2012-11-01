63130beeabdd9792d0895e683bc2f3fbd653190a
Nicolas Landier <nicolas@landier.org>
2012-11-01 16:19:51 +0100

    Initialization

---
+++ test/model/test_email.py
@@ -0,0 +1,10 @@
+import unittest
+from mail.model.email import Email
+
+class EmailTestCase(unittest.TestCase):
+    def test_repr(self):
+        email = Email(7, "This is my subject")
+        self.assertEqual("7: This is my subject", email.__repr__())
+
+if __name__ == '__main__':
+    unittest.main()
