059778d3d3cddc102a836e88b3b4adbb5798fe67
Nicolas Landier <nicolas@landier.org>
2012-11-02 19:36:34 +0100

    Changed setup.py + moved test folder into mail module

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
-
-version = '0.1.0'
-
-setup(name='mail',
-    version=version,
-    description="This is a description of the package",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-        # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='mail, Gmail, CouchDB',
-    author='Nicolas Landier',
-    author_email='nicolas.landier@gmail.com',
-    url='http://wiki.secondlife.com/wiki/Eventlet',
-    license='MIT',
-    packages=find_packages('mail'),
-    install_requires=['couchdb'],
+setup(
+    name = 'Mail',
+    version = '0.1.0',
+    author = 'Nicolas Landier',
+    author_email = 'mescouilles@surton.nez',
+    packages = ['mail', 'mail.test'],
+    scripts = ['bin/mail.py'],
+    url = '',
+    license =  open('LICENSE.txt').read(),
+    description = 'Grabs emails and store them into a CouchDB.',
+    long_description = open('README.rst').read(),
+    install_requires = ['couchdb'],
 )
