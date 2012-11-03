16f5fdcc36c8c48e3fc0fb1129db939af23b0b02
Nicolas Landier <nicolas@landier.org>
2012-11-03 11:33:15 +0100

    Search using Gmail expressions and fetching work.

--- mail/service/grabber.py
+++ mail/service/grabber.py
@@ -1,50 +1,69 @@
 import imaplib
 import email
+from pprint import pprint
 
 class Grabber(object):
     def __init__(self, config, dao):
         self.config = config
         self.dao = dao
 
+
     def run(self):
         mailBox = self.connectEmailAccount()
         self.retrieveEmails(mailBox)
 
+
     def connectEmailAccount(self):
         mailBox = imaplib.IMAP4_SSL(self.config.IMAP['Host'])
         mailBox.login(self.config.IMAP['Username'], self.config.IMAP['Password'])
-        mailBox.list()
+        pprint(mailBox.list())
         mailBox.select(self.config.IMAP['Folder'])
         return mailBox
 
-    def retrieveEmails(self, mailBox):
-        result, data = mailBox.uid('search', None, self.config.IMAP['Search'])
-        latest_email_uid = data[0].split()[-1]
-        result, data = mailBox.uid('fetch', latest_email_uid, '(RFC822)')
-        uid = data[0][0]
-        raw_email = data[0][1]
-        mail = email.message_from_string(raw_email)
 
-        cleanEmail = self.convertEmailToDictionary(mail)
-        cleanEmail['_id'] = uid
-        self.dao.save(cleanEmail)
+    def retrieveEmails(self, mailBox):
+
+        result, data = mailBox.uid('search', None, self.config.IMAP['Search'])
+        uidList = data[0].replace(' ', ',')
+        result, data = mailBox.uid('fetch', uidList, self.config.IMAP['DataFormat'])
+
+        for i in range(0, len(data), 2):
+            mail = self.parseEmail(data[i])
+            pprint(mail)
+            self.dao.save(mail)
+
+
+    def parseEmail(self, input):
+        raw_message = email.message_from_string(input[1])
+
+        # This part should be refactored using RegEx to doesn't count on order.
+        ids = input[0].replace('(', '').split()
+        setattr(raw_message, '_id', ids[4]) # X-GM-MSGID to be the CouchDB _id
+        setattr(raw_message, ids[1], ids[2]) # X-GM-THRID
+        setattr(raw_message, ids[3], ids[4]) # X-GM-MSGID
+        setattr(raw_message, ids[5], ids[6]) # UID
+
+        return self.convertEmailToDictionary(raw_message)
+
 
     def convertEmailToDictionary(self, email):
-        cleanEmail = email.__dict__
+        dictEmail = email.__dict__
 
-        cleanEmail['headers'] = cleanEmail['_headers']
-        del cleanEmail['_headers']
-        cleanEmail['payload'] = cleanEmail['_payload']
-        del cleanEmail['_payload']
-        cleanEmail['charset'] = cleanEmail['_charset']
-        del cleanEmail['_charset']
-        cleanEmail['default_type'] = cleanEmail['_default_type']
-        del cleanEmail['_default_type']
-        cleanEmail['unixfrom'] = cleanEmail['_unixfrom']
-        del cleanEmail['_unixfrom']
+        # We can remove not wanted fields here.
+        dictEmail['headers'] = dictEmail['_headers']
+        del dictEmail['_headers']
+        dictEmail['payload'] = dictEmail['_payload']
+        del dictEmail['_payload']
+        dictEmail['charset'] = dictEmail['_charset']
+        del dictEmail['_charset']
+        dictEmail['default_type'] = dictEmail['_default_type']
+        del dictEmail['_default_type']
+        dictEmail['unixfrom'] = dictEmail['_unixfrom']
+        del dictEmail['_unixfrom']
 
-        if not isinstance(cleanEmail['payload'], str):
-            for payload in cleanEmail['payload']:
-                payload = self.convertEmailToDictionary(payload)
+        # Convert recursively payloads (messages) in this email.
+        if isinstance(dictEmail['payload'], list):
+            for i in range(0, len(dictEmail['payload'])):
+                dictEmail['payload'][i] = self.convertEmailToDictionary(dictEmail['payload'][i])
 
-        return cleanEmail
+        return dictEmail
