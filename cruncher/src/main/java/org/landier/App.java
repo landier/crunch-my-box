601479fa0e67b71816aa9e0d0a76cc28e5243671
Nicolas Landier <nicolas@landier.org>
2012-12-29 11:51:48 +0100

    Added cruncher

---
+++ cruncher/src/main/java/org/landier/App.java
@@ -0,0 +1,13 @@
+package org.landier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
